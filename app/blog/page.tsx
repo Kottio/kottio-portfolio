7b79835ba310ff8806716952038b1d429923d0af
Thomas Cottiaux <thcottiaux@gmail.com>
2025-06-08 20:28:33 +0200

    ui

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,7 +7,7 @@ export default function BlogPage() {
     <>
 
       <div className="">
-        <section className="mx-10 px-10 flex justify-between items-center border-x-1 border-b-1">
+        <section className="mx-10 px-10 flex justify-between items-center border-x-1 border-b-1 ">
 
           <span className="text-4xl">KOTTIO <span className="font-bold">BLOG</span></span>
           <div className="flex gap-10 h-full">
@@ -38,79 +38,83 @@ export default function BlogPage() {
 
         <section className="px-10 flex ">
 
-          <div className="border-b-1 border-l-1 w-90 h-100  flex flex-col  ">
+          <div className="border-b-1 border-l-1 w-120   flex flex-col  hover:mt-3 hover:border-t-1 ">
             <div className=" h-70 w-full flex flex-col items-center justify-center ">
               <div className="relative h-19/20 w-19/20">
                 <Image src={"/Tunesia-22.jpg"} alt={"Hey"} fill className="object-cover " />
               </div>
             </div>
 
-            <span className="text-xl  px-2" >Data Engineering in Impact</span>
-            <ul className=" px-2 h-8 flex gap-1  ">
-              <Badge variant="developer">DEV</Badge>
-              <Badge variant="photo">PHOTO</Badge>
+            <span className="text-xl pl-2 py-3  " >DATA ENGINEERING IN IMPACT</span>
+            <ul className="  h-10  border-y-1 flex  items-center text-black">
+              <li className="bg-red-100 h-full w-15 border-r-1 flex items-center justify-center">DEV</li>
+              <li className="bg-cyan-100 h-full w-20 flex   border-r-1  items-center justify-center">PHOTO</li>
+
             </ul>
 
-            <span className="px-2 pt-2">Modern Day data pipeline transform how impact change the world... </span>
-            {/* <span className="text-sm px-2 text-[#639FA1]">June 20, 2025</span> */}
+            <span className="px-2 pt-2 min-h-20 pb-3">Modern Day data pipeline transform how impact change the world... </span>
+
+            <div className="flex justify-between items-center px-2">
+              <span className="">06/20/25</span>
+              <svg width="35" height="35" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14 34L34 14M34 14H14M34 14V34" stroke="#1E1E1E" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            </div>
+
           </div>
-
-
-
-          <div className=" border-l-1 border-b-1 w-90 h-100  flex flex-col  ">
+          <div className="border-b-1 border-l-1 w-120   flex flex-col  hover:mt-3 hover:border-t-1 ">
             <div className=" h-70 w-full flex flex-col items-center justify-center ">
               <div className="relative h-19/20 w-19/20">
-                <Image src={"/Tunesia-22.jpg"} alt={"Hey"} fill className="object-cover " />
+                <Image src={"/tree.jpg"} alt={"Hey"} fill className="object-cover " />
               </div>
             </div>
 
-            <span className="text-xl  px-2" >Data Engineering in Impact</span>
-            <ul className=" px-2 h-8 flex gap-1  ">
-              <Badge variant="developer">DEV</Badge>
-              <Badge variant="photo">PHOTO</Badge>
+            <span className="text-xl pl-2 py-3  " >DEVELOPPING YOUR OWN APP</span>
+            <ul className="  h-10  border-y-1 flex  items-center text-black">
+              <li className="bg-red-100 h-full w-15 border-r-1 flex items-center justify-center">DEV</li>
+              <li className="bg-orange-100 h-full w-20 flex   border-r-1  items-center justify-center">MUSIC</li>
+
             </ul>
 
-            <span className="px-2 pt-2">Modern Day data pipeline transform how impact change the world... </span>
-            {/* <span className="text-sm px-2 text-[#639FA1]">June 20, 2025</span> */}
+            <span className="px-2 pt-2 min-h-20 pb-3">Modern Day data pipeline transform how impact change the world... </span>
+
+            <div className="flex justify-between items-center px-2">
+              <span className="">06/20/25</span>
+              <svg width="35" height="35" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14 34L34 14M34 14H14M34 14V34" stroke="#1E1E1E" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            </div>
+
           </div>
 
-
-
-          <div className=" border-l-1 border-b-1 w-90 h-100  flex flex-col  ">
+          <div className="border-b-1 border-x-1 w-120   flex flex-col  hover:mt-3 hover:border-t-1 ">
             <div className=" h-70 w-full flex flex-col items-center justify-center ">
               <div className="relative h-19/20 w-19/20">
-                <Image src={"/Tunesia-22.jpg"} alt={"Hey"} fill className="object-cover " />
+                <Image src={"/fog1.jpg"} alt={"Hey"} fill className="object-cover " />
               </div>
             </div>
 
-            <span className="text-xl  px-2" >Data Engineering in Impact</span>
-            <ul className=" px-2 h-8 flex gap-1  ">
-              <Badge variant="developer">DEV</Badge>
-              <Badge variant="photo">PHOTO</Badge>
+            <span className="text-xl pl-2 py-3  " >DEVELOPPING YOUR OWN APP</span>
+            <ul className="  h-10  border-y-1 flex  items-center text-black">
+              <li className="bg-red-100 h-full w-15 border-r-1 flex items-center justify-center">DEV</li>
+
+
             </ul>
 
-            <span className="px-2 pt-2">Modern Day data pipeline transform how impact change the world... </span>
-            {/* <span className="text-sm px-2 text-[#639FA1]">June 20, 2025</span> */}
-          </div>
+            <span className="px-2 pt-2 min-h-20 pb-3">Modern Day data pipeline transform how impact change the world... </span>
 
-
-
-          <div className=" border-x-1 border-b-1 w-90 h-100  flex flex-col  ">
-            <div className=" h-70 w-full flex flex-col items-center justify-center ">
-              <div className="relative h-19/20 w-19/20">
-                <Image src={"/Tunesia-22.jpg"} alt={"Hey"} fill className="object-cover " />
-              </div>
+            <div className="flex justify-between items-center px-2">
+              <span className="">06/20/25</span>
+              <svg width="35" height="35" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14 34L34 14M34 14H14M34 14V34" stroke="#1E1E1E" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
             </div>
 
-            <span className="text-xl  px-2" >Data Engineering in Impact</span>
-            <ul className=" px-2 h-8 flex gap-1  ">
-              <Badge variant="developer">DEV</Badge>
-              <Badge variant="photo">PHOTO</Badge>
-            </ul>
-
-            <span className="px-2 pt-2">Modern Day data pipeline transform how impact change the world... </span>
-            {/* <span className="text-sm px-2 text-[#639FA1]">June 20, 2025</span> */}
           </div>
+
+
+
+
 
         </section>
       </div>
